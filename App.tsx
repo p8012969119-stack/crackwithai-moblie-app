8cbadab68e50655c50f9c4bde3944488479af036
p8012969119-stack <p8012969119@gmail.com>
2026-09-24 00:44:56 +0530

    Push all configuration files and application code to main branch

--- App.tsx
+++ App.tsx
@@ -1,45 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
+import React from 'react';
+import { StatusBar } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/store/AuthContext';
+import { RootNavigator } from './src/navigation/RootNavigator';
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
-import {
-  SafeAreaProvider,
-  useSafeAreaInsets,
-} from 'react-native-safe-area-context';
-
-function App() {
-  const isDarkMode = useColorScheme() === 'dark';
-
+export default function App() {
   return (
     <SafeAreaProvider>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <AppContent />
+      <NavigationContainer>
+        <AuthProvider>
+          <StatusBar barStyle="dark-content" />
+          <RootNavigator />
+        </AuthProvider>
+      </NavigationContainer>
     </SafeAreaProvider>
   );
 }
-
-function AppContent() {
-  const safeAreaInsets = useSafeAreaInsets();
-
-  return (
-    <View style={styles.container}>
-      <NewAppScreen
-        templateFileName="App.tsx"
-        safeAreaInsets={safeAreaInsets}
-      />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-/**
- * @format
- */
-
 import { AppRegistry } from 'react-native';
 import App from './App';
-import { name as appName } from './app.json';
+import appConfig from './app.json';
+
+const appName = appConfig?.name || 'CrackWithAI';
 
 AppRegistry.registerComponent(appName, () => App);
+
